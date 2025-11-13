3856bf4b888a4e9b80462602b942e9e0386d3c34
Emma <EmmaRussellKilburn@gmail.com>
2025-11-13 12:05:04 +0000

    add coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,10 @@ export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
+      coverage: {
+        provider: "v8",
+        include: ["src/**/*.{ts,js,vue}"]
+      },
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
