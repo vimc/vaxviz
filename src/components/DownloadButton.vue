5c369300d9c304f3ed08dfffe2e9cd4b8e4da3e0
David Mears <clarinet.david@gmail.com>
2026-03-19 12:43:16 +0000

    Rephrase file labels; remove 'hiding filters' feature

--- src/components/DownloadButton.vue
+++ src/components/DownloadButton.vue
@@ -22,75 +22,124 @@
       </div>
     </template>
     <template #body>
-      <div class="flex flex-col gap-y-4 leading-relaxed">
-        <p>
-          These downloadable summary tables contain the mean and median estimates, as well as 95% confidence intervals, of the impact ratios from each set of model runs. All diseases are included where applicable.
-        </p>
-        <p>
-          By default, the file(s) relevant to the current plot view are pre-selected, but you can select any combination of files to download, either using the filters below to filter the options, or by individually selecting the files from the drop-down.
-        </p>
+      <div>
+        <div class="mb-8 flex flex-col leading-relaxed gap-y-4">
+          <p>
+            These downloadable summary tables contain the mean and median estimates, as well as 95% confidence intervals, of the impact ratios from each set of model runs. All diseases are included where applicable.
+          </p>
+          <p>
+            By default, the file(s) relevant to the current plot view are pre-selected, but you can select any combination of files to download, either using the filters below to filter the options, or by individually selecting the files from the drop-down.
+          </p>
+        </div>
         <div class="flex gap-20">
           <div class="flex flex-col gap-4 w-full">
-            <div>
-              <label id="downloadSelectLabel" for="toDownloadSelect" class="sr-only">
-                Select files to download
-              </label>
-              <VueSelect
-                id="toDownloadSelect"
-                v-model="toDownload"
-                :is-multi="true"
-                :is-clearable="toDownload.length > 1"
-                :is-menu-open="menuOpen"
-                :hide-selected-options="false"
-                :close-on-select="false"
-                :options="fileOptions"
-                aria-labelledby="downloadSelectLabel"
-                @menu-closed="menuOpen = false"
-                @menu-opened="menuOpen = true"
-              >
-                <template #placeholder>
-                  <span class="text-sm">Select files to download</span>
-                </template>
-              </VueSelect>
-            </div>
-            <FwbButton
-              @click="doDownload(toDownload)"
-              color="default"
-              class="cursor-pointer mt-auto w-fit"
-              :disabled="toDownload.length < 1"
+            <label
+              id="downloadSelectLabel"
+              for="toDownloadSelect"
+              class="font-medium text-lg"
             >
-              <span class="flex items-center gap-2 justify-center">
-                <DownloadIcon class="size-4" />
-                Download {{ toDownload.length }} selected file{{ toDownload.length > 1 ? 's' : '' }} (X KB)
-              </span>
-            </FwbButton>
-            <FwbButton
-              @click="doDownload(dataStore.allPossibleSummaryTableFilenames)"
-              color="light"
-              class="cursor-pointer w-fit"
+              Select files to download
+            </label>
+            <VueSelect
+              id="toDownloadSelect"
+              v-model="toDownload"
+              :is-multi="true"
+              :is-clearable="toDownload.length > 1"
+              :is-menu-open="menuOpen"
+              :hide-selected-options="false"
+              :close-on-select="false"
+              :options="fileOptions"
+              aria-labelledby="downloadSelectLabel"
+              @menu-closed="menuOpen = false"
+              @menu-opened="menuOpen = true"
             >
-              <span class="flex items-center gap-2 justify-center">
-                <DownloadIcon class="size-4" />
-                Download all summary tables (Y KB)
-              </span>
-            </FwbButton>
-          </div>
-          <div class="min-w-150">
-            <div class="flex items-center gap-4">
+              <template #placeholder>
+                <span class="text-sm">Select files to download</span>
+              </template>
+            </VueSelect>
+            <div class="flex flex-wrap gap-4">
               <FwbButton
-                color="light"
-                class="cursor-pointer w-50"
-                @click="onShowHideFilters(filtersOpen)"
+                @click="doDownload(toDownload)"
+                color="default"
+                class="cursor-pointer mt-auto w-fit"
+                :disabled="toDownload.length < 1"
               >
                 <span class="flex items-center gap-2 justify-center">
-                  <svg class="size-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M18.796 4H5.204a1 1 0 0 0-.753 1.659l5.302 6.058a1 1 0 0 1 .247.659v4.874a.5.5 0 0 0 .2.4l3 2.25a.5.5 0 0 0 .8-.4v-7.124a1 1 0 0 1 .247-.659l5.302-6.059c.566-.646.106-1.658-.753-1.658Z"/>
-                  </svg>
-                  {{ filtersOpen ? 'Hide and clear filters' : 'Show filters' }}
+                  <DownloadIcon class="size-4" />
+                  Download {{ toDownload.length }} selected file{{ toDownload.length > 1 ? 's' : '' }} (X KB)
                 </span>
               </FwbButton>
               <FwbButton
-                v-if="filtersOpen"
+                @click="doDownload(dataStore.allPossibleSummaryTableFilenames)"
+                color="light"
+                class="cursor-pointer w-fit"
+              >
+                <span class="flex items-center gap-2 justify-center">
+                  <DownloadIcon class="size-4" />
+                  Download all summary tables (Y KB)
+                </span>
+              </FwbButton>
+            </div>
+          </div>
+          <div class="min-w-150 flex flex-col gap-4">
+            <h3 class="flex items-center gap-1 font-medium text-lg">
+              <svg class="size-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M18.796 4H5.204a1 1 0 0 0-.753 1.659l5.302 6.058a1 1 0 0 1 .247.659v4.874a.5.5 0 0 0 .2.4l3 2.25a.5.5 0 0 0 .8-.4v-7.124a1 1 0 0 1 .247-.659l5.302-6.059c.566-.646.106-1.658-.753-1.658Z"/>
+              </svg>
+              Filters
+            </h3>
+            <div class="flex flex-wrap gap-6">
+              <fieldset class="gap-5 w-fit">
+                <legend class="block text-sm mb-2">Burden metrics:</legend>
+                <div>
+                  <FwbCheckbox
+                    v-for="metric in metricOptions"
+                    :key="metric.value"
+                    v-model="burdenMetricFilter[metric.value]"
+                    :label="metric.label"
+                    :wrapper-class="'w-fit'"
+                  />
+                </div>
+              </fieldset>
+              <fieldset class="gap-5 w-fit">
+                <legend class="block text-sm mb-2">Geographical resolutions:</legend>
+                <div>
+                  <FwbCheckbox
+                    v-for="geog in Object.keys(geogFilter)"
+                    :key="geog"
+                    v-model="geogFilter[geog]"
+                    :label="geog"
+                    :wrapper-class="'w-fit'"
+                  />
+                </div>
+              </fieldset>
+              <fieldset class="gap-5 w-fit">
+                <legend class="block text-sm mb-2">Scales:</legend>
+                <div>
+                  <FwbCheckbox
+                    v-for="scale in Object.keys(scaleFilter)"
+                    :key="scale"
+                    v-model="scaleFilter[scale]"
+                    :label="scale"
+                    :wrapper-class="'w-fit'"
+                  />
+                </div>
+              </fieldset>
+              <fieldset class="gap-5 w-fit">
+                <legend class="block text-sm mb-2">Split by activity type:</legend>
+                <div>
+                  <FwbCheckbox
+                    v-for="splitBy in Object.keys(activityTypeFilter)"
+                    :key="splitBy"
+                    v-model="activityTypeFilter[splitBy]"
+                    :label="splitBy"
+                    :wrapper-class="'w-fit'"
+                  />
+                </div>
+              </fieldset>
+            </div>
+            <div class="flex items-center gap-4 ml-auto">
+              <FwbButton
                 @click="selectAllFilesMatchingFilters"
                 color="default"
                 class="cursor-pointer w-fit"
@@ -99,58 +148,16 @@
                   Select all files matching filters
                 </span>
               </FwbButton>
-            </div>
-            <div :class="{ invisible: !filtersOpen }">
-              <div class="flex flex-wrap gap-6 my-10">
-                <fieldset class="gap-5 w-fit">
-                  <legend class="block text-sm mb-2">Burden metrics:</legend>
-                  <div>
-                    <FwbCheckbox
-                      v-for="metric in metricOptions"
-                      :key="metric.value"
-                      v-model="burdenMetricFilter[metric.value]"
-                      :label="metric.label"
-                      :wrapper-class="'w-fit'"
-                    />
-                  </div>
-                </fieldset>
-                <fieldset class="gap-5 w-fit">
-                  <legend class="block text-sm mb-2">Geographical resolutions:</legend>
-                  <div>
-                    <FwbCheckbox
-                      v-for="geog in Object.keys(geogFilter)"
-                      :key="geog"
-                      v-model="geogFilter[geog]"
-                      :label="geog"
-                      :wrapper-class="'w-fit'"
-                    />
-                  </div>
-                </fieldset>
-                <fieldset class="gap-5 w-fit">
-                  <legend class="block text-sm mb-2">Scales:</legend>
-                  <div>
-                    <FwbCheckbox
-                      v-for="scale in Object.keys(scaleFilter)"
-                      :key="scale"
-                      v-model="scaleFilter[scale]"
-                      :label="scale"
-                      :wrapper-class="'w-fit'"
-                    />
-                  </div>
-                </fieldset>
-                <fieldset class="gap-5 w-fit">
-                  <legend class="block text-sm mb-2">Split by activity type:</legend>
-                  <div>
-                    <FwbCheckbox
-                      v-for="splitBy in Object.keys(activityTypeFilter)"
-                      :key="splitBy"
-                      v-model="activityTypeFilter[splitBy]"
-                      :label="splitBy"
-                      :wrapper-class="'w-fit'"
-                    />
-                  </div>
-                </fieldset>
-              </div>
+              <FwbButton
+                color="light"
+                class="cursor-pointer w-50"
+                :disabled="filtersAreClear"
+                @click="clearFilters"
+              >
+                <span class="flex items-center gap-2 justify-center">
+                  Clear filters
+                </span>
+              </FwbButton>
             </div>
           </div>
         </div>
@@ -174,7 +181,6 @@ const helpInfoStore = useHelpInfoStore();
 
 const downloadModalVisible = ref(false);
 const menuOpen = ref(false);
-const filtersOpen = ref(false);
 
 const handleModalClose = () => {
   // If the user presses Escape in order to close the VueSelect component, do not interpret this as
@@ -188,25 +194,25 @@ const fileLabel = (fileName: string) => {
   const fileLabelParts = [];
   metricOptions.forEach((metric) => {
     if (fileName.includes(metric.value)) {
-      fileLabelParts.push(metric.label);
+      fileLabelParts.push(`${metric.label.split(" ")[0]} impact ratios`);
     }
   });
   if (fileName.includes(LocResolution.COUNTRY)) {
-    fileLabelParts.push("by country");
+    fileLabelParts.push(" by country");
   } else if (fileName.includes(LocResolution.SUBREGION)) {
-    fileLabelParts.push("by subregion");
+    fileLabelParts.push(" by subregion");
   } else {
-    fileLabelParts.push("(global)")
+    fileLabelParts.push(" globally")
   }
   if (fileName.includes(Dimension.ACTIVITY_TYPE)) {
-    fileLabelParts.push("split by activity type");
+    fileLabelParts.push(", split by activity type,");
   }
   if (fileName.includes("log")) {
-    fileLabelParts.push("in log-10 scale");
+    fileLabelParts.push(" in log-10 scale");
   } else {
-    fileLabelParts.push("in linear scale");
+    fileLabelParts.push(" in linear scale");
   }
-  return `${fileLabelParts.join(" ")} (1.23 KB)`; // TODO: Replace with actual file size.
+  return `${fileLabelParts.join("")} (1.23 KB)`; // TODO: Replace with actual file size.
 };
 
 
@@ -302,19 +308,18 @@ const doDownload = async (files: string[]) => {
   // await dataStore.downloadSummaryTables(files);
 };
 
-const updateAllFilters = async (select: boolean) => {
+const filtersAreClear = computed(() => {
+  return filtersAndMatchers.every(([filter]) => {
+    return Object.values(filter.value).every(filteredIn => filteredIn === false);
+  });
+});
+
+const clearFilters = () => {
   filtersAndMatchers.forEach(([filter]) => {
     Object.keys(filter.value).forEach(key => {
-      filter.value[key] = select;
+      filter.value[key] = false;
     });
   });
-};
-
-const onShowHideFilters = (open: boolean) => {
-  filtersOpen.value = !open;
-  if (!filtersOpen.value) {
-    updateAllFilters(false);
-  }
 };
 
 const selectAllFilesMatchingFilters = () => {
