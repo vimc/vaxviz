485451d90c2ff4b9623a752318480fc42403bd60
Emma <EmmaRussellKilburn@gmail.com>
2026-03-31 15:52:38 +0100

    unit test

---
+++ tests/unit/components/EmbargoNotice.spec.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect, beforeEach, vi } from 'vitest';
+import EmbargoNotice from "../../../src/components/EmbargoNotice.vue";
+
+describe("EmbargoNotice component", () => {
+  it("shows expected embargo notice", () => {
+
+  });
+});
