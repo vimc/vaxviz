4c4b1f35fd97012369840118b74c7cce4ebaae6f
David Mears <clarinet.david@gmail.com>
2026-05-11 15:07:05 +0100

    Add missing e2e test for location downloads

--- tests/e2e/utils.ts
+++ tests/e2e/utils.ts
@@ -1,4 +1,4 @@
-import { expect, Page } from "@playwright/test";
+import { expect, Download, Page } from "@playwright/test";
 
 export const selectFocus = async (page: Page, optionLabel: string) => {
   await page.click(".dropdown-icon");
@@ -6,4 +6,21 @@ export const selectFocus = async (page: Page, optionLabel: string) => {
   await option.scrollIntoViewIfNeeded();
   await expect(option).toBeVisible();
   await option.click();
+};
+
+// Download the file(s) that are pre-selected by default, based on the plot controls
+export const doDownload = async (page: Page, expectedNumberOfFiles: number): Promise<Download> => {
+  const downloadPromise = page.waitForEvent("download");
+  await page.getByRole("button", { name: "Downloads" }).click();
+  await page.getByRole('button', { name: "Other downloads" }).click();
+  await page.getByRole("button", { name: `Download ${expectedNumberOfFiles}` }).click();
+
+  const download = await downloadPromise;
+
+  // Wait for the download process to complete
+  await download.path();
+
+  await page.getByRole("button", { name: "close", exact: true }).click();
+
+  return download;
 };
