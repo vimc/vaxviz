3d44638cbdd4644fa21e8550c81b1f4cc6fb7f79
David Mears <60350599+david-mears-2@users.noreply.github.com>
2026-05-13 19:17:51 +0100

    Update src/components/PrivacyModal.vue
    Co-authored-by: Emma Russell <44669576+EmmaLRussell@users.noreply.github.com>

--- src/components/PrivacyModal.vue
+++ src/components/PrivacyModal.vue
@@ -16,7 +16,7 @@
           We do not use cookies.
         </p>
         <p>
-          For these analytical purposes, we collect data on page views, user behaviour, session duration, device type, and location (country).
+          For these analytical purposes, we collect data on page views, user behaviour (downloads and plot control updates), session duration, device type, and location (country).
           This data is statistically aggregated and does not identify or track individual users across sessions.
         </p>
         <p>
